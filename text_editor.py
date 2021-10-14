7f99a7c415330e9bde0397a51e83a31a677b55da
sampathkumarlanka92 <sampathkumarlanka92@gmail.com>
2021-10-14 14:01:15 +0530

    Text Editor Project Completed

--- text_editor.py
+++ text_editor.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 # from tkinter import *
 from tkinter import filedialog, messagebox
 from tkinter import font, colorchooser
+import os
 
 root = tk.Tk()
 root.title("Text Editor")
@@ -56,7 +57,7 @@ colors_dict = {
     "Default" : ("#000000", "#ffffff"),
     "Light Red" : ("#2d2d2d", "#ffcccb"),
     "Night Blue" : ("#ededed", "#6b9dc2"),
-    "Dark" : ("#c4c4c4c", "#2d2d2d"),
+    "Dark" : ("#c4c4c4", "#2d2d2d"),
 }
 
 # End Colortheme Menu
@@ -137,40 +138,335 @@ texteditor.pack(fill=tk.BOTH, expand=True)
 scrollbar.config(command=texteditor.yview)
 texteditor.config(yscrollcommand=scrollbar.set)
 
+# fontfamily and fontsize functionas
+cur_fontfam = 'Arial'
+cur_fontsize = 12
+
+def changefont(event=None):
+    global cur_fontfam
+    cur_fontfam = fontfamily.get()
+    texteditor.configure(font=(cur_fontfam, cur_fontsize))
+
+def changefontsize(event=None):
+    global cur_fontsize
+    cur_fontsize = sizevar.get()
+    texteditor.configure(font=(cur_fontfam, cur_fontsize))
+
+
+fontbox.bind("<<ComboboxSelected>>", changefont)
+fontsize.bind("<<ComboboxSelected>>", changefontsize)
+
+# buttons functions
+# bold button functionality
+def changebold():
+    textproperty = tk.font.Font(font=texteditor['font'])
+    if textproperty.actual()['weight'] == 'normal':
+        texteditor.configure(font=(cur_fontfam, cur_fontsize, 'bold'))
+    if textproperty.actual()['weight'] == 'bold':
+        texteditor.configure(font=(cur_fontfam, cur_fontsize, 'normal'))
+
+boldbtn.configure(command=changebold)
+
+# italic functionlaity
+def changeitalic():
+    textproperty = tk.font.Font(font=texteditor['font'])
+    if textproperty.actual()['slant'] == 'roman':
+        texteditor.configure(font=(cur_fontfam, cur_fontsize, 'italic'))
+    if textproperty.actual()['slant'] == 'italic':
+        texteditor.configure(font=(cur_fontfam, cur_fontsize, 'normal'))
+
+italicbtn.configure(command=changeitalic)
+
+# underline functionality
+def changeunderline():
+    textproperty = tk.font.Font(font=texteditor['font'])
+    if textproperty.actual()['underline'] == 0:
+        texteditor.configure(font=(cur_fontfam, cur_fontsize, 'underline'))
+    if textproperty.actual()['underline'] == 1:
+        texteditor.configure(font=(cur_fontfam, cur_fontsize, 'normal'))
+
+underlinebtn.configure(command=changeunderline)
+
+## font color functionality
+def changefontcolor():
+    colorvar = tk.colorchooser.askcolor()
+    texteditor.configure(fg=colorvar[1])
+
+
+colorchooserbtn.configure(command=changefontcolor)
+
+### align functionality
+
+def alignleft():
+    textcontent = texteditor.get(1.0, 'end')
+    texteditor.tag_config('left', justify=tk.LEFT)
+    texteditor.delete(1.0, tk.END)
+    texteditor.insert(tk.INSERT, textcontent, 'left')
+
+leftalingbtn.configure(command=alignleft)
+
+## center
+def aligncenter():
+    textcontent = texteditor.get(1.0, 'end')
+    texteditor.tag_config('center', justify=tk.CENTER)
+    texteditor.delete(1.0, tk.END)
+    texteditor.insert(tk.INSERT, textcontent, 'center')
+
+justifybtn.configure(command=aligncenter)
+
+## right
+def alignright():
+    textcontent = texteditor.get(1.0, 'end')
+    texteditor.tag_config('right', justify=tk.RIGHT)
+    texteditor.delete(1.0, tk.END)
+    texteditor.insert(tk.INSERT, textcontent, 'right')
+
+rightalingbtn.configure(command=alignright)
+
+texteditor.configure(font=('Arial', 12))
+
 # ---- End Text Editor ----
 
 
 # ---- Status Bar ----
+statusbar = ttk.Label(root, text='Status Bar')
+statusbar.pack(side=tk.BOTTOM)
+
+textchanged = False
+def changed(event=None):
+    global textchanged
+    if texteditor.edit_modified():
+        textchanged = True
+        words = len(texteditor.get(1.0, 'end-1c').split())
+        characters = len(texteditor.get(1.0, 'end-1c'))
+        statusbar.config(text=f'Characters : {characters} Words : {words}')
+    texteditor.edit_modified(False)
+
+texteditor.bind('<<Modified>>', changed)
 
 # ---- End Status Bar ----
 
 
 # ---- Main menu Functions ----
+## variable
+url = ''
+
 # file commands
-file.add_command(label="New", image=newicon, compound=tk.LEFT, accelerator='Ctrl+N')
-file.add_command(label="Open", image=openicon, compound=tk.LEFT, accelerator='Ctrl+O')
-file.add_command(label="Save", image=saveicon, compound=tk.LEFT, accelerator='Ctrl+S')
-file.add_command(label="Save As", image=saveasicon, compound=tk.LEFT, accelerator='Ctrl+Alt+S')
-file.add_command(label="Exit", image=exiticon, compound=tk.LEFT, accelerator='Ctrl+Q')
+## new functionality
+def newfile(event=None):
+    global url
+    url = ''
+    texteditor.delete(1.0, tk.END)
+
+file.add_command(label="New", image=newicon, compound=tk.LEFT, accelerator='Ctrl+N', command=newfile)
+
+## open functionality
+
+def openfile(event=None):
+    global url
+    url = filedialog.askopenfilename(initialdir=os.getcwd(), title='Select File', filetypes=(('Text File', '*.txt'), ('All files', '*.*')))
+    try:
+        with open(url, 'r') as fr:
+            texteditor.delete(1.0, tk.END)
+            texteditor.insert(1.0, fr.read())
+    except FileNotFoundError:
+        return
+    except:
+        return
+    root.title(os.path.basename(url))
+
+file.add_command(label="Open", image=openicon, compound=tk.LEFT, accelerator='Ctrl+O', command=openfile)
+
+## save file
+
+def savefile(event=None):
+    global url
+    try:
+        if url:
+            content = str(texteditor.get(1.0, tk.END))
+            with open(url, 'w', encoding='utf-8') as fw:
+                fw.write(content)
+        else:
+            url = filedialog.asksaveasfile(mode = 'w', defaultextension='.txt', filetypes=(('Text File', '*.txt'), ('All files', '*.*')))
+            content2 = texteditor.get(1.0, tk.END)
+            url.write(content2)
+            url.close()
+    except:
+        return
+
+file.add_command(label="Save", image=saveicon, compound=tk.LEFT, accelerator='Ctrl+S', command=savefile)
+
+## save as functionality
+def saveas(event=None):
+    global url
+    try:
+        content = texteditor.get(1.0, tk.END)
+        url = filedialog.asksaveasfile(mode = 'w', defaultextension='.txt', filetypes=(('Text File', '*.txt'), ('All files', '*.*')))
+        url.write(content)
+        url.close()
+    except:
+        return
+
+file.add_command(label="Save As", image=saveasicon, compound=tk.LEFT, accelerator='Ctrl+Alt+S', command=saveas)
+
+## exit functionality
+
+def exitfunc(event=None):
+    global url, textchanged
+    try:
+        if textchanged:
+            mbox = messagebox.askyesnocancel('Warning', 'Do you want to save the file ?')
+            if mbox is True:
+                if url:
+                    content = texteditor.get(1.0, tk.END)
+                    with open(url, 'w', encoding='utf-8') as fw:
+                        fw.write(content)
+                        root.destroy()
+                else:
+                    content2 = str(texteditor.get(1.0, tk.END))
+                    url = filedialog.asksaveasfile(mode = 'w', defaultextension='.txt', filetypes=(('Text File', '*.txt'), ('All files', '*.*')))
+                    url.write(content2)
+                    url.close()
+                    root.destroy()
+            elif mbox is False:
+                root.destroy()
+        else:
+            root.destroy()
+    except:
+        return
+
+file.add_command(label="Exit", image=exiticon, compound=tk.LEFT, accelerator='Ctrl+Q', command=exitfunc)
+
+
+############ find functionality
+
+def findfunc(event=None):
+
+    def find():
+        word = findinput.get()
+        texteditor.tag_remove('match', '1.0', tk.END)
+        matches = 0
+        if word:
+            startpos = '1.0'
+            while True:
+                startpos = texteditor.search(word, startpos, stopindex=tk.END)
+                if not startpos:
+                    break
+                endpos = f'{startpos}+{len(word)}c'
+                texteditor.tag_add('match', startpos, endpos)
+                matches += 1
+                startpos = endpos
+                texteditor.tag_config('match', foreground='red', background='yellow')
+
+    def replace():
+        word = findinput.get()
+        replacetext = replaceinput.get()
+        content = texteditor.get(1.0, tk.END)
+        newcontent = content.replace(word, replacetext)
+        texteditor.delete(1.0, tk.END)
+        texteditor.insert(1.0, newcontent)
+
+    finddialogue = tk.Toplevel()
+    finddialogue.geometry('450x250+500+200')
+    finddialogue.title('Find')
+    finddialogue.resizable(0,0)
+
+    ## frame
+    findframe = ttk.LabelFrame(finddialogue, text='Find/Replace')
+    findframe.pack(pady=20)
+
+    ## labels
+    textfindlabel = ttk.Label(findframe, text='Find : ')
+    textreplacelabel = ttk.Label(findframe, text= 'Replace')
+
+    ## entry
+    findinput = ttk.Entry(findframe, width=30)
+    replaceinput = ttk.Entry(findframe, width=30)
+
+    ## button
+    findbutton = ttk.Button(findframe, text='Find', command=find)
+    replacebutton = ttk.Button(findframe, text= 'Replace', command=replace)
+
+    ## label grid
+    textfindlabel.grid(row=0, column=0, padx=4, pady=4)
+    textreplacelabel.grid(row=1, column=0, padx=4, pady=4)
+
+    ## entry grid
+    findinput.grid(row=0, column=1, padx=4, pady=4)
+    replaceinput.grid(row=1, column=1, padx=4, pady=4)
+
+    ## button grid
+    findbutton.grid(row=2, column=0, padx=8, pady=4)
+    replacebutton.grid(row=2, column=1, padx=8, pady=4)
+
+    finddialogue.mainloop()
+
 
 # edit commands
-edit.add_command(label="Copy", image=copyicon, compound=tk.LEFT, accelerator="Ctrl+C")
-edit.add_command(label="Cut", image=cuticon, compound=tk.LEFT, accelerator="Ctrl+X")
-edit.add_command(label="Paste", image=pasteicon, compound=tk.LEFT, accelerator="Ctrl+V")
-edit.add_command(label="Clear All", image=clearallicon, compound=tk.LEFT, accelerator="Ctrl+Alt+X")
-edit.add_command(label="Find", image=findicon, compound=tk.LEFT, accelerator="Ctrl+F")
+edit.add_command(label="Copy", image=copyicon, compound=tk.LEFT, accelerator="Ctrl+C", command=lambda:texteditor.event_generate("<Control c>"))
+edit.add_command(label="Cut", image=cuticon, compound=tk.LEFT, accelerator="Ctrl+X", command=lambda:texteditor.event_generate("<Control x>"))
+edit.add_command(label="Paste", image=pasteicon, compound=tk.LEFT, accelerator="Ctrl+V", command=lambda:texteditor.event_generate("<Control v>"))
+edit.add_command(label="Clear All", image=clearallicon, compound=tk.LEFT, accelerator="Ctrl+Alt+X", command= lambda:texteditor.delete(1.0, tk.END))
+edit.add_command(label="Find", image=findicon, compound=tk.LEFT, accelerator="Ctrl+F", command = findfunc)
+
+
+## view check button
+
+showstatusbar = tk.BooleanVar()
+showstatusbar.set(True)
+showtoolbar = tk.BooleanVar()
+showtoolbar.set(True)
+
+def hidetoolbar():
+    global showtoolbar
+    if showtoolbar:
+        toolbar.pack_forget()
+        showtoolbar = False
+    else :
+        texteditor.pack_forget()
+        statusbar.pack_forget()
+        toolbar.pack(side=tk.TOP, fill=tk.X)
+        texteditor.pack(fill=tk.BOTH, expand=True)
+        statusbar.pack(side=tk.BOTTOM)
+        showtoolbar = True
+
+
+def hidestatusbar():
+    global showstatusbar
+    if showstatusbar:
+        statusbar.pack_forget()
+        showstatusbar = False
+    else :
+        statusbar.pack(side=tk.BOTTOM)
+        showstatusbar = True
+
 
 # view commands
-view.add_checkbutton(label="Tool Bar", image=toolbaricon, compound=tk.LEFT)
-view.add_checkbutton(label="Status Bar", image=statusbaricon, compound=tk.LEFT)
+view.add_checkbutton(label="Tool Bar", onvalue=True, offvalue=0, variable = showtoolbar, image=toolbaricon, compound=tk.LEFT, command=hidetoolbar)
+view.add_checkbutton(label="Status Bar", onvalue=1, offvalue=False, variable = showstatusbar, image=statusbaricon, compound=tk.LEFT, command=hidestatusbar)
 
 # colortheme
+## color theme
+def changetheme():
+    chosentheme = themechoice.get()
+    colortuple = colors_dict.get(chosentheme)
+    fgcolor, bgcolor = colortuple[0], colortuple[1]
+    texteditor.config(background=bgcolor, fg=fgcolor)
+
 count = 0
 for color in colors_dict:
-    colortheme.add_radiobutton(label=color, image=colors[count], variable=themechoice, compound=tk.LEFT)
+    colortheme.add_radiobutton(label=color, image=colors[count], variable=themechoice, compound=tk.LEFT, command=changetheme)
     count += 1
 
 # ---- End Main Menu Functions ----
 
 root.config(menu=mymenu)
+## bind shortcut keys
+root.bind("<Control-n>", newfile)
+root.bind("<Control-o>", openfile)
+root.bind("<Control-s>", savefile)
+root.bind("<Control-Alt-s>", saveas)
+root.bind("<Control-q>", exitfunc)
+root.bind("<Control-f>", findfunc)
+
 root.mainloop()
